f7ea472c1c96fde1bb6997aec2b11f1afbbec357
AMI-lesson <aceami113@gmail.com>
2025-06-06 15:57:45 +0900

    Add files via upload

--- ポートフォリオ/script.js
+++ ポートフォリオ/script.js
@@ -1,14 +1,28 @@
 $(function () {
+  // ハンバーガー
   $("#hamburger").on("click", function () {
     $(this).toggleClass("open");
     $("#gnav").stop().slideToggle();
 
-    // テキストの切り替え
     const label = $(this).find(".hamburger-label");
     if ($(this).hasClass("open")) {
       label.text("Close");
     } else {
       label.text("Menu");
+    }
+  });
+
+  // works表示制限
+  const visibleCount = 4; // 最初に表示する個数
+  const loadCount = 2; // クリックで追加表示する個数
+
+  $(".works-item").slice(visibleCount).addClass("hidden");
+
+  $(".more-button").on("click", function (e) {
+    e.preventDefault();
+    $(".works-item.hidden").slice(0, loadCount).removeClass("hidden");
+    if ($(".works-item.hidden").length === 0) {
+      $(this).fadeOut();
     }
   });
 });
