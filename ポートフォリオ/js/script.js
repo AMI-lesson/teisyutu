0b9e7ce66f947388affdd579cb52ed437daf0d40
AMI-lesson <aceami113@gmail.com>
2025-06-09 14:00:54 +0900

    Add files via upload

---
+++ ポートフォリオ/js/script.js
@@ -0,0 +1,50 @@
+$(function () {
+  // ハンバーガー
+  $("#hamburger").on("click", function () {
+    $(this).toggleClass("open");
+    $("#gnav").stop().slideToggle();
+
+    const label = $(this).find(".hamburger-label");
+    if ($(this).hasClass("open")) {
+      label.text("Close");
+    } else {
+      label.text("Menu");
+    }
+  });
+
+  // works表示制限
+  const visibleCount = 4; // 最初に表示する個数
+  const loadCount = 2; // クリックで追加表示する個数
+
+  $(".works__item").slice(visibleCount).addClass("hidden");
+
+  $(".works__more-button").on("click", function (e) {
+    e.preventDefault();
+    $(".works__item.hidden").slice(0, loadCount).removeClass("hidden");
+    if ($(".works__item.hidden").length === 0) {
+      $(this).fadeOut();
+    }
+  });
+
+  // スクロールしたときにボタンを表示
+  window.onscroll = function () {
+    scrollFunction();
+  };
+
+  function scrollFunction() {
+    const btn = document.getElementById("scrollToTopBtn");
+    if (
+      document.body.scrollTop > 20 ||
+      document.documentElement.scrollTop > 20
+    ) {
+      btn.style.display = "block";
+    } else {
+      btn.style.display = "none";
+    }
+  }
+
+  // ボタンをクリックしたときにトップに戻る
+  document.getElementById("scrollToTopBtn").onclick = function () {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+});
